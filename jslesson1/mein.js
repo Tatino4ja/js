e357345fdcb029cb7966a780062f1a59c7fe3230
Tatino4ja <tatino4ja@gmail.com>
2022-02-01 20:15:55 +0200

    js1home

---
+++ jslesson1/mein.js
@@ -0,0 +1,197 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+let zm1 = `hello`;
+let zm2 = 'owu';
+let zm3 = 'com';
+let zm4 = 'ua';
+let zm5 = 1;
+let zm6 = 10;
+let zm7 = -999;
+let zm8 = 123;
+let zm9 = 3.14;
+let zm10 = 2.7;
+let zm11 = 16;
+let zm12 = 'true';
+let zm13 = 'false';
+
+console.log(zm1);
+// alert(zm1);
+document.write(zm1);
+
+console.log(zm2);
+// alert(zm2);
+document.write(zm2);
+
+console.log(zm3);
+// alert(zm3);
+document.write(zm3);
+
+console.log(zm4);
+// alert(zm4);
+document.write(zm4);
+
+console.log(zm5);
+// alert(zm5);
+document.write(zm5);
+
+console.log(zm6);
+// alert(zm6);
+document.write(zm6);
+
+console.log(zm7);
+// alert(zm7);
+document.write(zm7);
+
+console.log(zm8);
+// alert(zm8);
+document.write(zm8);
+
+console.log(zm9);
+// alert(zm9);
+document.write(zm9);
+
+console.log(zm10);
+// alert(zm10);
+document.write(zm10);
+
+console.log(zm11);
+// alert(zm11);
+document.write(zm11);
+
+console.log(zm12);
+// alert(zm12);
+document.write(zm12);
+
+console.log(zm13);
+// alert(zm13);
+document.write(zm13);
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+zm1 = 'haha';
+console.log(zm1);
+// alert(zm1);
+document.write(zm1);
+
+zm2 = '123';
+console.log(zm2);
+// alert(zm2);
+document.write(zm2);
+
+zm3 = 869;
+console.log(zm3);
+// alert(zm3);
+document.write(zm3);
+
+zm4 = 2 + 3;
+console.log(zm4);
+// alert(zm4);
+document.write(zm4);
+
+zm5 = 'js';
+console.log(zm5);
+// alert(zm5);
+document.write(zm5);
+
+zm6 = Math.sqrt(9);
+console.log(zm6);
+// alert(zm6);
+document.write(zm6);
+
+zm7 = Math.pow(5, 2);
+console.log(zm7);
+// alert(zm7);
+document.write(zm7);
+
+zm8 = 5*9;
+console.log(zm8);
+// alert(zm8);
+document.write(zm8);
+
+zm9 = 6-3;
+console.log(zm9);
+// alert(zm9);
+document.write(zm9);
+
+zm10 = 10/2;
+console.log(zm10);
+// alert(zm10);
+document.write(zm10);
+
+zm11 = 6**2;
+console.log(zm11);
+// alert(zm11);
+document.write(zm11);
+
+zm12 = 9 ** (1/2);
+console.log(zm12);
+// alert(zm12);
+document.write(zm12)
+
+zm13 = 'hello' + ' ' +'okten';
+console.log(zm13)
+// alert(zm13);
+document.write(zm13);
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+let firstName = 'Dzundza';
+let middleName = ' Tetiana';
+let lastName = ' Ivanivna';
+
+let person = firstName + middleName + lastName;
+console.log(person);
+alert(person);
+document.write(person);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let nameT = prompt ('Імя?');
+// let nameI = prompt('По батькові');
+// let age = prompt('Вік?');
+// alert('Вітаю ' + nameT + ' ' + nameI + '. ' + 'Тобі' + ' ' + age + ' років.');
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+// 5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+
+console.log ( 5 < 6 );
+console.log ( 5 > 6);
+console.log ( 5 >= 6 );
+console.log ( 5 === 6 );
+console.log ( 10 === 10);
+console.log ( 10 >= 10 );
+console.log ( 10 < - 10 );
+console.log ( 10 > 10 );
+console.log ( 10 === -10);
+console.log ( 123 > '123');
+console.log ( 123 == '123');
+
+
+
+
