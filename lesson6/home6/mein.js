20b584fb8de87fc2b7be9cccb3b124677abe0ae0
Tatino4ja <tatino4ja@gmail.com>
2022-02-12 13:55:37 +0200

    js6home

--- lesson6/home6/mein.js
+++ lesson6/home6/mein.js
@@ -42,20 +42,58 @@
 //
 //
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
-// let array = []
+
+//
 // let str = 'Каждый охотник желает знать';
+// function stringToarray(arr) {
+//     let y = arr.split(' ')
+//     return y
+// }
+// stringToarray(str)
+// console.log(stringToarray(str))
+// document.writeln(str)
 
 
-
-// document.writeln(arr); // ['Каждый', 'охотник', 'желает', 'знать']
 //
+// let str = 'Каждый охотник желает знать';
+// let stringToarray = (arr)=> {
+//     let newArr = arr.split(' ')
+// }
+// stringToarray(str)
+// document.writeln(str); // ['Каждый', 'охотник', 'желает', 'знать']
+
+
+// - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної
+// кількості символів.
+
+
+// let string = 'Каждый охотник желает знать';
+// let delete_characters = (str) => str.substring(0, 6)
+// delete_characters(string)
+// document.writeln(delete_characters(string))
+
 //
-// - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
-//     let str = 'Каждый охотник желает знать';
-// document.writeln(delete_characters(str, 7)); // Каждый
+// let string = 'Каждый охотник желает знать';
+// let delete_characters = (str) => {
+//     let p = str.substring(0, 6)
+//     return p
+// }
+// delete_characters(string)
+// console.log(delete_characters(string))
+// document.write(delete_characters(string))
+// //
+
+
 //
 // - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
 //     let str = "HTML JavaScript PHP";
 // document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
 //
 // - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
+
+
+// let string = 'Каждый охотник желает знать';
+// let delete_characters = (str, len) =>{
+//     return str.substr(0, len)
+// }
+// document.writeln(delete_characters(string, 7))
